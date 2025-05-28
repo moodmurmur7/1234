45b840794869b42f4de4f4aa26ad9e0125ce5b38
moodmurmur7 <moodmurmur.care@gmail.com>
2025-05-28 13:46:27 +0530

    Implement all question types

--- src/components/questions/QuestionTypes/MultipleChoiceQuestion.tsx
+++ src/components/questions/QuestionTypes/MultipleChoiceQuestion.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Question } from '../../../types';
+import { MathRenderer } from '../../MathRenderer';
 
 interface MultipleChoiceQuestionProps {
   question: Question;
-  onAnswer: (answer: string) => void;
-  selectedOption?: string;
+  onAnswer: (optionIndex: number) => void;
+  selectedOption?: number;
   showCorrect?: boolean;
 }
 
@@ -19,57 +20,61 @@ export function MultipleChoiceQuestion({
   }
 
   return (
-    <div className="space-y-4">
-      <div className="font-medium text-gray-900">{question.text}</div>
-      <div className="space-y-2">
-        {question.options.map((option, index) => {
-          const isSelected = selectedOption === option;
-          const isCorrect = showCorrect && option === question.correctAnswer;
-          const isIncorrect = showCorrect && isSelected && option !== question.correctAnswer;
+    <div className="space-y-3">
+      {question.options.map((option, index) => {
+        const isSelected = selectedOption === index;
+        const isCorrect = showCorrect && index === question.correctOptionIndex;
+        const isIncorrect = showCorrect && isSelected && index !== question.correctOptionIndex;
 
-          return (
-            <button
-              key={index}
-              onClick={() => onAnswer(option)}
-              disabled={showCorrect}
-              className={`w-full text-left px-4 py-3 rounded-lg border transition-colors ${
+        return (
+          <button
+            key={option.id}
+            onClick={() => onAnswer(index)}
+            disabled={showCorrect}
+            className={`w-full text-left px-4 py-3 rounded-lg border transition-colors ${
+              isSelected
+                ? 'border-indigo-500 bg-indigo-50'
+                : 'border-gray-200 hover:border-gray-300'
+            } ${
+              isCorrect
+                ? 'bg-green-50 border-green-500'
+                : isIncorrect
+                ? 'bg-red-50 border-red-500'
+                : ''
+            }`}
+          >
+            <div className="flex items-center space-x-3">
+              <div className={`w-4 h-4 rounded-full border ${
                 isSelected
-                  ? 'border-blue-500 bg-blue-50'
-                  : 'border-gray-200 hover:border-gray-300'
+                  ? 'border-indigo-500 bg-indigo-500'
+                  : 'border-gray-300'
               } ${
                 isCorrect
-                  ? 'bg-green-50 border-green-500'
+                  ? 'border-green-500 bg-green-500'
                   : isIncorrect
-                  ? 'bg-red-50 border-red-500'
+                  ? 'border-red-500 bg-red-500'
                   : ''
-              }`}
-            >
-              <div className="flex items-center space-x-3">
-                <div className={`w-4 h-4 rounded-full border ${
-                  isSelected
-                    ? 'border-blue-500 bg-blue-500'
-                    : 'border-gray-300'
+              }`} />
+              <div className="flex items-center">
+                <span className={`${
+                  isSelected ? 'text-indigo-700' : 'text-gray-700'
                 } ${
-                  isCorrect
-                    ? 'border-green-500 bg-green-500'
-                    : isIncorrect
-                    ? 'border-red-500 bg-red-500'
-                    : ''
-                }`} />
-                <span className={`${isSelected ? 'text-blue-700' : 'text-gray-700'} ${
                   isCorrect
                     ? 'text-green-700'
                     : isIncorrect
                     ? 'text-red-700'
                     : ''
                 }`}>
-                  {option}
+                  {option.text}
                 </span>
+                {option.latex && (
+                  <MathRenderer latex={option.latex} className="ml-2" />
+                )}
               </div>
-            </button>
-          );
-        })}
-      </div>
+            </div>
+          </button>
+        );
+      })}
     </div>
   );
 }
